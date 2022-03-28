43bc81aa00d9e0811f25f8c6fff7c669fa856682
tansantktk <tansantktk@hotmail.co.jp>
2022-03-28 15:51:28 +0900

    update: UI transitions

--- src/article/Article.tsx
+++ src/article/Article.tsx
@@ -1,6 +1,6 @@
 import { News } from "../types/news";
 import './Article.css';
-import { Chip } from "@mui/material";
+import { Chip, Zoom, Slide, Grow } from "@mui/material";
 
 export function Article (props: News) {
     const clickSourceBy = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
@@ -8,20 +8,26 @@ export function Article (props: News) {
         window.open(props.scrapedUrl, '_blank')
     }
 
+    const transitionDelay = () => Math.floor(Math.random() * 400) + 'ms';
+
     return (
-        <article>
-            { props.articleImageUrl && 
-                <a href={props.articleUrl}>
-                    <div className="image">
-                        <img src={props.articleImageUrl} alt="" />
-                    </div>
-                </a>
-            }
-            <div className="title">
-                <a href={props.articleUrl}>{props.title}</a>
-            </div>            
-            <div className="scrapedDateTime">{props.scrapedDateTime}</div>
-            <Chip label={props.sourceBy} size="small" onClick={clickSourceBy} />
-        </article>
+        <div>
+            <Zoom in={true} style={{ transitionDelay: transitionDelay() }}>
+                <article>
+                    { props.articleImageUrl && 
+                        <a href={props.articleUrl}>
+                            <div className="image">
+                                <img src={props.articleImageUrl} alt="" />
+                            </div>
+                        </a>
+                    }
+                    <div className="title">
+                        <a href={props.articleUrl}>{props.title}</a>
+                    </div>            
+                    <div className="scrapedDateTime">{props.scrapedDateTime}</div>
+                    <Chip label={props.sourceBy} size="small" onClick={clickSourceBy} />
+                </article>
+            </Zoom>
+        </div>
     );
 }
