dd9989001ca0170d750eb6fcde3a3da326b13c48
TansanMilMil <tmdev2929@gmail.com>
2025-04-26 14:34:35 +0900

    update: api endpoint

--- src/api/birdsEyeApi.ts
+++ src/api/birdsEyeApi.ts
@@ -5,20 +5,19 @@ import { News } from "../types/news";
 import { NewsReaction } from "../types/newsReaction";
 
 export class BirdsEyeApi {
-    private static readonly API_ENDPOINT: string = Env.BirdsEyeApiEndpoint();
+  private static readonly API_ENDPOINT: string = Env.BirdsEyeApiEndpoint();
 
-    public static getTodayNews(): Promise<AxiosResponse<News[], any>> {
-        return axios.get(`${this.API_ENDPOINT}/news/today-news`);
-    }
+  public static getTodayNews(): Promise<AxiosResponse<News[], any>> {
+    return axios.get(`${this.API_ENDPOINT}/news/today-news`);
+  }
 
-    public static getTrends(): Promise<AxiosResponse<News[], any>> {
-        return axios.get(`${this.API_ENDPOINT}/news/trends`);
-    }
+  public static getTrends(): Promise<AxiosResponse<News[], any>> {
+    return axios.get(`${this.API_ENDPOINT}/news/trends`);
+  }
 
-    public static getReactions(id: number): Promise<AxiosResponse<NewsReaction[], any>> {
-        const query = {
-            id: id
-        };
-        return axios.get(`${this.API_ENDPOINT}/news/news-reactions`, { params: query });
-    }        
+  public static getReactions(
+    id: number
+  ): Promise<AxiosResponse<NewsReaction[], any>> {
+    return axios.get(`${this.API_ENDPOINT}/news/news-reactions/${id}`);
+  }
 }
